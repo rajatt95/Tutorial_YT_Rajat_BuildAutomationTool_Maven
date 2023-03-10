420c0a9d19cd5250b8bcd7b271772736497c59a8
“Rajat <rajatvermaa95@gmail.com>
2023-03-10 11:26:14 +0530

    Maven | 02 - Project Structure | pom.xml

---
+++ src/main/java/org/learning/App.java
@@ -0,0 +1,28 @@
+/**
+ * @author Rajat Verma
+ * Email: rajatvermaa95@gmail.com
+ * Github Profile: https://github.com/rajatt95
+ * Github Page: https://rajatt95.github.io/
+ * LinkedIn: https://www.linkedin.com/in/rajatt95/
+ * Topmate: https://topmate.io/rajatt95
+ * Telegram: https://t.me/rajatt95
+ * Instagram: https://www.instagram.com/rajattvermaa95/
+ * Youtube: https://www.youtube.com/@rajatt95
+ * WhatsApp: https://chat.whatsapp.com/DVRP0ViD0YwELUKF2XMJHy
+ */
+
+/***************************************************/
+
+package org.learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/learning/AppTest.java
@@ -0,0 +1,54 @@
+/**
+ * @author Rajat Verma
+ * Email: rajatvermaa95@gmail.com
+ * Github Profile: https://github.com/rajatt95
+ * Github Page: https://rajatt95.github.io/
+ * LinkedIn: https://www.linkedin.com/in/rajatt95/
+ * Topmate: https://topmate.io/rajatt95
+ * Telegram: https://t.me/rajatt95
+ * Instagram: https://www.instagram.com/rajattvermaa95/
+ * Youtube: https://www.youtube.com/@rajatt95
+ * WhatsApp: https://chat.whatsapp.com/DVRP0ViD0YwELUKF2XMJHy
+ */
+
+/***************************************************/
+
+
+//package org.learning;
+//
+//import junit.framework.Test;
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
+//
+///**
+// * Unit test for simple App.
+// */
+//public class AppTest
+//    extends TestCase
+//{
+//    /**
+//     * Create the test case
+//     *
+//     * @param testName name of the test case
+//     */
+//    public AppTest( String testName )
+//    {
+//        super( testName );
+//    }
+//
+//    /**
+//     * @return the suite of tests being tested
+//     */
+//    public static Test suite()
+//    {
+//        return new TestSuite( AppTest.class );
+//    }
+//
+//    /**
+//     * Rigourous Test :-)
+//     */
+//    public void testApp()
+//    {
+//        assertTrue( true );
+//    }
+//}
